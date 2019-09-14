0e2f2577afc5579bb77132ac0b2bbeab63027686
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-09-14 16:54:54 +0900

    🎨 Add "--test" option to README generator script

--- script/readme-generator.js
+++ script/readme-generator.js
@@ -7,22 +7,67 @@ const Mustache = require('mustache');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-async function main(templatePath) {
+async function tryReadFile(...args) {
+  try {
+    return await readFileAsync(...args);
+  } catch (error) {
+    return null;
+  }
+}
+
+function parseArgs(args) {
+  const options = new Map();
+  const targets = [];
+
+  let optname;
+  for (const arg of args) {
+    if (/^--/.test(arg)) {
+      optname = arg.substring(2);
+      options.set(optname, true);
+    } else if (/^-/.test(arg)) {
+      for (const optchar of arg.substring(1)) {
+        optname = optchar;
+        options.set(optname, true);
+      }
+    } else if (optname) {
+      options.set(optname, arg);
+      optname = null;
+    } else {
+      targets.push(arg);
+    }
+  }
+
+  return { options, targets };
+}
+
+async function main(args) {
+  const cwd = process.cwd();
+  const { options } = parseArgs(args);
+  const templatePath = path.resolve(
+    cwd,
+    options.get('template') || 'README.mustache',
+  );
   const templateCode = await readFileAsync(templatePath, 'utf8');
   const view = {
-    pkg: require(path.resolve(process.cwd(), 'package.json')),
-    pkgLock: require(path.resolve(process.cwd(), 'package-lock.json')),
+    pkg: require(path.resolve(cwd, 'package.json')),
+    pkgLock: require(path.resolve(cwd, 'package-lock.json')),
   };
   const output = Mustache.render(templateCode, view);
-  const outputPath = path.join(path.dirname(templatePath), 'README.md');
-  await writeFileAsync(outputPath, output);
+  const outputPath = path.resolve(path.dirname(templatePath), 'README.md');
+
+  if (options.has('test')) {
+    const origReadme = await tryReadFile(outputPath);
+    if (origReadme && !origReadme.equals(Buffer.from(output))) {
+      throw new Error(`Do not edit ${path.relative(cwd, outputPath)} manually`);
+    }
+  } else {
+    await writeFileAsync(outputPath, output);
+  }
 }
 
 (async () => {
   try {
-    await main(
-      process.argv[2] || path.resolve(process.cwd(), 'README.mustache'),
-    );
+    await main(process.argv.slice(2));
   } catch (err) {
     process.exitCode = 1;
     console.error(err);
